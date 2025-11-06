24bd2818e76ab45bff5ab14e0d853684ca3bed65
been-360 <shay.mittal1@gmail.com>
2025-11-05 21:34:11 -0600

    added main.ino

---
+++ main.ino
@@ -0,0 +1,25 @@
+
+void setup() {
+    typedef struct {
+    int rightjoyX;
+    int rightjoyY;
+    int leftjoyX;
+    int leftjoyY;
+    int lefttrigger;
+    int righttrigger;
+    bool Ybutton;
+    bool Xbutton;
+    bool Bbutton;
+    bool Abutton;
+    bool Backbutton;
+    bool Startbutton;
+    bool Dpadup;
+    bool Dpadleft;
+    bool Dpadright;
+    bool Dpaddown;
+} controller;
+}
+
+void loop() {
+    
+}
