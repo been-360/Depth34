9330c857216e635a5562858f9a920323bdfdacd4
been-360 <shay.mittal1@gmail.com>
2026-01-25 17:48:48 -0600

    Created new init function for Pwm, added debugging to Pwm loop, made gamepad async

--- controller/src/tasks/gamepad.rs
+++ controller/src/tasks/gamepad.rs
@@ -1,13 +1,38 @@
 use std::time::Duration;
 
-use tokio::sync::watch;
 use gilrs::{Axis, Button, EventType, Gilrs};
+use tokio::sync::watch;
 
-use crate::{logs::red, state::{L, R, State}};
+use crate::{
+    logs::red,
+    state::{L, Quad, R, Special, State},
+};
 
-pub fn gamepad_loop(sender: watch::Sender<State>) {
+pub async fn gamepad_loop(sender: watch::Sender<State>) {
     let mut axes = std::collections::HashMap::new();
     let mut gil = Gilrs::new().unwrap();
+    const DEADZONE: f32 = 0.05;
+
+    let mut state = State {
+        l: L { lx: 0.0, ly: 0.0 },
+        r: R { rx: 0.0, ry: 0.0 },
+        dpad: Quad {
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        },
+        face: Quad {
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        },
+        special: Special {
+            mode: false,
+        }
+    };
+
 
     for (_id, gamepad) in gil.gamepads() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
@@ -16,50 +41,57 @@ pub fn gamepad_loop(sender: watch::Sender<State>) {
     loop {
         while let Some(e) = gil.next_event() {
             match e.event {
-                EventType::ButtonPressed(button, _) => match button {
-                    Button::North => println!("Press N"),
-                    Button::East => println!("Press E"),
-                    Button::South => println!("Press S"),
-                    Button::West => println!("Press W"),
-                    Button::DPadDown => println!("Press DPad Down"),
-                    Button::DPadLeft => println!("Press DPad Left"),
-                    Button::DPadUp => println!("Press DPad Up"),
-                    Button::DPadRight => println!("Press DPad Right"),
-                    _ => {}
-                },
-                EventType::ButtonReleased(button, _) => match button {
-                    Button::North => println!("Release N"),
-                    Button::East => println!("Release E"),
-                    Button::South => println!("Release S"),
-                    Button::West => println!("Release W"),
-                    Button::DPadDown => println!("Release DPad Down"),
-                    Button::DPadLeft => println!("Release DPad Left"),
-                    Button::DPadUp => println!("Release DPad Up"),
-                    Button::DPadRight => println!("Release DPad Right"),
-                    _ => {}
-                },
+                EventType::ButtonPressed(button, _) => {
+                    match button {
+                        Button::North => state.face.up = true,
+                        Button::East => state.face.right = true,
+                        Button::South => state.face.down = true,
+                        Button::West => state.face.left = true,
+                        Button::DPadUp => state.dpad.up = true,
+                        Button::DPadDown => state.dpad.down = true,
+                        Button::DPadLeft => state.dpad.left = true,
+                        Button::DPadRight => state.dpad.right = true,
+                        Button::Mode => state.special.mode = true,
+                        _ => println!("Pressed unknown button: {:?}", button),
+                    }
+                }
+                EventType::ButtonReleased(button, _) => {
+                    match button {
+                        Button::North => state.face.up = false,
+                        Button::East => state.face.right = false,
+                        Button::South => state.face.down = false,
+                        Button::West => state.face.left = false,
+                        Button::DPadUp => state.dpad.up = false,
+                        Button::DPadDown => state.dpad.down = false,
+                        Button::DPadLeft => state.dpad.left = false,
+                        Button::DPadRight => state.dpad.right = false,
+                        Button::Mode => state.special.mode = false,
+                        _ => println!("Released unknown button: {:?}", button),
+                    }
+                }
                 EventType::AxisChanged(axis, val, _) => {
                     axes.insert(axis, val);
 
-                    let lx = *axes.get(&Axis::LeftStickX).unwrap_or(&0.0);
-                    let ly = *axes.get(&Axis::LeftStickY).unwrap_or(&0.0);
-                    let rx = *axes.get(&Axis::RightStickX).unwrap_or(&0.0);
-                    let ry = *axes.get(&Axis::RightStickY).unwrap_or(&0.0);
+                    state.l.lx = *axes.get(&Axis::LeftStickX).unwrap_or(&0.0);
+                    state.l.ly = *axes.get(&Axis::LeftStickY).unwrap_or(&0.0);
+                    state.r.rx = *axes.get(&Axis::RightStickX).unwrap_or(&0.0);
+                    state.r.ry = *axes.get(&Axis::RightStickY).unwrap_or(&0.0);
 
-                    let state = State {
-                        l: L { lx, ly },
-                        r: R { rx, ry },
-                    };
-
-                    if sender.send(state).is_err() {
-                        eprintln!("{}", red("An error occured while updating the state"));
-                    }
+                    state.l.lx = if state.l.lx.abs() < DEADZONE { 0.0 } else { state.l.lx };
+                    state.l.ly = if state.l.ly.abs() < DEADZONE { 0.0 } else { state.l.ly };
+                    state.r.rx = if state.r.rx.abs() < DEADZONE { 0.0 } else { state.r.rx };
+                    state.r.ry = if state.r.ry.abs() < DEADZONE { 0.0 } else { state.r.ry };
                 }
 
                 _ => {}
             }
+
+            if sender.send(state).is_err() {
+                eprintln!("{}", red("An error occured while updating the state"));
+            }
+
         }
-        
-        std::thread::sleep(Duration::from_millis(10));
+
+        tokio::time::sleep(Duration::from_millis(10)).await
     }
 }
--- controller/src/tasks/pwm.rs
+++ controller/src/tasks/pwm.rs
@@ -1,27 +1,92 @@
 use tokio::sync::watch;
 
+use crate::logs::{green, red};
+use crate::pigpio;
+use crate::pigpio::servo;
 use crate::state::State;
 
-pub async fn pwm_loop(reciever: watch::Receiver<State>) {
+pub async fn pwm_loop(pwm: Pwm,reciever: watch::Receiver<State>) {
     let mut rec = reciever.clone();
+    
 
     loop {
         rec.changed().await.unwrap();
         let state = *rec.borrow();
+        pwm.esc(&state).await;
+
+        println!("{state:?}")
     }
 }
 
 pub struct Pwm {
+    rov_up1: u32,
+    rov_up2: u32,
+    rov_m1: u32,
+    rov_m2: u32,
+    rov_m3: u32,
+    rov_m4: u32,
+    stop: u32,
 }
 
 impl Pwm {
-    pub async fn new() -> Result<Self, apigpio::Error> {
-        // --------- \\
-
-        // TODO: Replace unwrap with error handling `here
-
-        Ok(Self {  })
+    pub async fn new() -> Self {
+        Self {
+            rov_up1: 17,
+            rov_up2: 27,
+            rov_m1: 5,
+            rov_m2: 6,
+            rov_m3: 19,
+            rov_m4: 26,
+            stop: 1500,
+        }
     }
 
-    pub fn setup(&self) {}
+    pub async fn init(&self) {
+        match pigpio::initialize() {
+            Ok(_) => {
+                println!("{}", green("PiGPIO has been initialized"))
+            }
+            Err(e) => {
+                eprintln!(
+                    "{}: {e}",
+                    red("PiGPIO has failed to initialize with error code")
+                )
+            }
+        }
+
+        let pins = [
+            self.rov_m1,
+            self.rov_m2,
+            self.rov_m3,
+            self.rov_m4,
+            self.rov_up1,
+            self.rov_up2,
+        ];
+        for pin in pins {
+            servo(pin, 1500);
+        }
+    }
+
+    pub async fn esc(&self, state: &State) {
+        servo(self.rov_up1, self.vertical_math(1).await);
+    }
+
+    async fn vertical_math(&self, motor: u8) -> u32 {
+        let mut ms = self.stop;
+        
+        let pwm = self.clamp(ms).await;
+
+        pwm
+    }
+
+    async fn clamp(&self, mut pwm: u32) -> u32 {
+        if pwm < 500 {
+            pwm = 500;
+        }
+        if pwm > 2500 {
+            pwm = 2500;
+        }
+
+        pwm
+    }
 }
