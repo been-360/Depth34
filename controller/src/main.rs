199e8771f39b1499b30d9408812a6390fa7f7847
been-360 <shay.mittal1@gmail.com>
2026-01-25 17:47:53 -0600

    feat(controller): add support for quadstick and special buttons

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,38 +1,48 @@
+mod logs;
 mod pigpio;
 mod state;
 mod tasks;
-mod logs;
 
-use crate::{state::{L, R, State}, tasks::pwm::pwm_loop};
+use crate::{
+    state::{L, Quad, R, Special, State},
+    tasks::pwm::{Pwm, pwm_loop},
+};
 
-use tokio::{spawn, sync::watch, task::spawn_blocking};
-use tasks::gamepad::gamepad_loop;
-use std::time::Duration;
 use logs::{green, red};
+use std::time::Duration;
+use tasks::gamepad::gamepad_loop;
+use tokio::{spawn, sync::watch, task::spawn_blocking};
 
 #[tokio::main]
 async fn main() {
-    match pigpio::initialize() {
-        Ok(_) => {
-            println!("{}", green("PiGPIO has been initialized"))
-        }
-        Err(e) => {
-            eprintln!(
-                "{}: {e}",
-                red("PiGPIO has failed to initialize with error code")
-            )
-        }
-    }
+    let pwm = Pwm::new().await;
+    pwm.init().await;
 
     let l = L { lx: 0.0, ly: 0.0 };
     let r = R { rx: 0.0, ry: 0.0 };
-    let state = State { l, r };
+    let spec_default = Special {
+        mode: false,
+    };
+    let quad_default = Quad {
+        up: false,
+        down: false,
+        left: false,
+        right: false,
+    };
+
+    let state = State {
+        l,
+        r,
+        dpad: quad_default,
+        face: quad_default,
+        special: spec_default,
+    };
     let (sender, reciever) = watch::channel(state);
 
-    let _gamepad_task = spawn_blocking(move || gamepad_loop(sender));
-    let _pwm_task = spawn(pwm_loop(reciever));
+    let _gamepad_task = spawn(gamepad_loop(sender));
+    let _pwm_task = spawn(pwm_loop(pwm, reciever));
 
-    tokio::time::sleep(Duration::from_millis(5000)).await;
-
-
+    loop {
+        tokio::time::sleep(Duration::from_secs(60)).await;
+    }
 }
--- controller/src/state.rs
+++ controller/src/state.rs
@@ -1,18 +1,34 @@
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct State {
     pub l: L,
     pub r: R,
+    pub dpad: Quad,
+    pub face: Quad,
+    pub special: Special,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct L {
     pub lx: f32,
     pub ly: f32,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct R {
     pub rx: f32,
     pub ry: f32,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Quad {
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Special {
+    pub mode: bool,
 }
