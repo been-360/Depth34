74b6249a68ab44f790d792f7123c422f105dfe7d
Shay <shay.mittal1@gmail.com>
2026-02-04 20:08:45 -0600

    Add todo and initialize pico-firmware project

--- pi-firmware/src/tasks/reflect.rs
+++ pi-firmware/src/tasks/reflect.rs
@@ -3,7 +3,7 @@ use tokio::net::{TcpStream};
 use futures::{SinkExt, StreamExt};
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
-
+// TODO: Complete reflect websockets
 
 pub async fn handle_reflect(stream: TcpStream) {
     let ws_stream = accept_async(stream).await.unwrap();
